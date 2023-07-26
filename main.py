4c339dd385ef9e95767346730fb3dad35d3fdef7
yegor-kirovich <73740454+yegor-kirovich@users.noreply.github.com>
2023-07-26 13:26:39 +0600

    Сделал конвертатор в символы(каждый пиксель в символ)

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 from PIL import Image
 
+
+def separate(pixel):
+    global separator
+    for i in range(len(separator)):
+        if separator[i] <= pixel[0] <= separator[i + 1]:
+            return list_of_symbols[i]
+
+
 image = Image.open("image.jpg")
 list_of_pixels = list(image.getdata())
+list_of_symbols = [".", ",", "'", "+", "*", "~", "=", "!", "/", "?", "%", "$", "#", "&", "@"]
+symbol_image = [[]]
+
+separator = [int(i * (256 / len(list_of_symbols))) for i in range(len(list_of_symbols) + 1)]
+
+cur_line = 0
+
+for pixel in list_of_pixels:
+    if len(symbol_image[cur_line]) == image.width:
+        cur_line += 1
+        symbol_image.append([])
+    symbol_image[cur_line].append(separate(pixel))
+
+for line in symbol_image:
+    for pixel in line:
+        print(pixel, end="")
+    print()
